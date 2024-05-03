2216336095724409a137a954339701c9b90367ed
Per Christian Moen <per.christian.moen@nav.no>
2024-05-03 20:47:12 +0200

    Sorterer nedtrekksmenyer

--- app/components/behandlinger-table/BehandlingerTable.tsx
+++ app/components/behandlinger-table/BehandlingerTable.tsx
@@ -37,9 +37,9 @@ export default function BehandlingerTable(props: Props) {
     setSearchParams(searchParams)
   }
 
-  function statusOptions(medLabel: boolean) {
+  function statusOptions() {
     return <Select
-      label={null}
+      label="Behandlingsstatus"
       defaultValue={searchParams.get('status') || undefined}
       onChange={(value) => {
         searchParams.set('status', value.target.value)
@@ -47,28 +47,29 @@ export default function BehandlingerTable(props: Props) {
           preventScrollReset: true,
         })
       }}
+      hideLabel
     >
       <option value=''>Alle statuser</option>
-      <option value='FEILENDE'>Feilende</option>
       <option value='DEBUG'>Debug</option>
+      <option value='FEILENDE'>Feilende</option>
+      <option value='FULLFORT'>Fullført</option>
+      <option value='OPPRETTET'>Opprettet</option>
       <option value='STOPPET'>Stoppet</option>
       <option value='UNDER_BEHANDLING'>Under behandling</option>
-      <option value='OPPRETTET'>Opprettet</option>
-      <option value='FULLFORT'>Fullført</option>
     </Select>
   }
 
   function behandlingtypeOptions() {
     let ekstraBehandlingType
     let currentBehandlingType = searchParams.get('behandlingType')
-    if (currentBehandlingType && !props.behandlingerResponse.behandlingTyper.sort((a,b) => 0 - (a > b ? -1 : 1)).includes(currentBehandlingType)) {
+    if (currentBehandlingType && !props.behandlingerResponse.behandlingTyper.includes(currentBehandlingType)) {
       ekstraBehandlingType = (<option value={currentBehandlingType}>{decodeBehandling(currentBehandlingType)}</option>)
     } else {
       ekstraBehandlingType = (<></>)
     }
 
     return <Select
-      label={null}
+      label="Behandlingstype"
       defaultValue={searchParams.get('behandlingType') || undefined}
       onChange={(value) => {
         searchParams.set('behandlingType', value.target.value)
@@ -76,6 +77,7 @@ export default function BehandlingerTable(props: Props) {
           preventScrollReset: true,
         })
       }}
+      hideLabel
     >
       <option value=''>Alle typer</option>
       {
@@ -83,7 +85,7 @@ export default function BehandlingerTable(props: Props) {
       }
 
       { props.behandlingerResponse.behandlingTyper ?
-        props.behandlingerResponse.behandlingTyper.map((type) => {
+        props.behandlingerResponse.behandlingTyper.sort((a,b) => a.localeCompare(b)).map((type) => {
         return (<option key={type} value={type}>{decodeBehandling(type)}</option>)
       })
         : <></>
@@ -114,7 +116,7 @@ export default function BehandlingerTable(props: Props) {
               <Table.ColumnHeader sortable sortKey="behandlingId" style={{ borderBottomWidth: 0, paddingBottom: 0 }}>
                 BehandlingId
               </Table.ColumnHeader>
-              <Table.ColumnHeader sortable sortKey="type" style={{ borderBottomWidth: 0, paddingBottom: 0 }}>
+              <Table.ColumnHeader sortable sortKey="class" style={{ borderBottomWidth: 0, paddingBottom: 0 }}>
                 Type
               </Table.ColumnHeader>
               <Table.ColumnHeader sortable sortKey="opprettet" style={{ borderBottomWidth: 0, paddingBottom: 0 }}>
@@ -144,7 +146,7 @@ export default function BehandlingerTable(props: Props) {
               </Table.DataCell>
               <Table.DataCell style={{ paddingTop: 0 }}>
                 {props.visStatusSoek ?
-                  statusOptions(false)
+                  statusOptions()
                   : <></>}
               </Table.DataCell>
             </Table.Row>
