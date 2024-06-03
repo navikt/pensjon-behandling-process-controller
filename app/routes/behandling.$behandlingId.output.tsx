d9dbcb5dcbeac9ed0bb9865fdf612bc539abcf77
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2024-06-03 13:37:08 +0200

    Oppdatert nedlastinglenke
    Co-authored-by: Yun Huang <yun.huang@nav.no>

--- app/routes/behandling.$behandlingId.output.tsx
+++ app/routes/behandling.$behandlingId.output.tsx
@@ -8,7 +8,8 @@ import {
 
 import invariant from 'tiny-invariant'
 import { requireAccessToken } from '~/services/auth.server'
-import { Box, CopyButton } from '@navikt/ds-react'
+import { Accordion, Box, CopyButton, Link } from '@navikt/ds-react'
+import { useEffect, useState } from 'react'
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   invariant(params.behandlingId, 'Missing behandlingId param')
@@ -33,16 +34,55 @@ export default function BehandlingOutput() {
     temp += JSON.stringify(JSON.parse(verdi), null, 4) + ','
   ))
 
+  const [downloadLink, setDownloadLink] = useState('')
+  const makeTextFile = () => {
+    // This creates the file.
+    // In my case, I have an array, and each item in
+    // the array should be on a new line, which is why
+    // I use .join('\n') here.
+    const data = new Blob(["["+temp+"]"], { type: 'application/json' })
+
+    // this part avoids memory leaks
+    if (downloadLink !== '') window.URL.revokeObjectURL(downloadLink)
+
+    // update the download link state
+    setDownloadLink(window.URL.createObjectURL(data))
+  }
+  // Call the function if list changes
+  useEffect(() => {
+    makeTextFile()
+  }, [temp])
+
   return (
     <>
-      <CopyButton copyText={"["+temp+"]"} />
-      <Box background='surface-info-subtle' id='outputbox'>
-        {output.str.map((verdi: string, index: number) => (<>
-            {JSON.stringify(JSON.parse(verdi), null, 4)},
-            <br />
-          </>
-        ))}
-      </Box>
+      <div style={{display: 'flex'}}>
+        <Link
+          style={{padding: 1 + 'em'}}
+          // this attribute sets the filename
+          download='liste.json'
+          // link to the download URL
+          href={downloadLink}
+        >
+          download json fil
+        </Link>
+      </div>
+      <Accordion>
+        <Accordion.Item>
+          <Accordion.Header>Json liste</Accordion.Header>
+          <Accordion.Content>
+            <CopyButton copyText={'[' + temp + ']'} />
+            <Box background='surface-info-subtle' id='outputbox'>
+              {output.str.map((verdi: string, index: number) => (<>
+                  {JSON.stringify(JSON.parse(verdi), null, 4)},
+                  <br />
+                </>
+              ))}
+            </Box>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion>
+
+
     </>
   )
 }
