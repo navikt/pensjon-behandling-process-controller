51172ce720ea1ee981650ad906f9fee8c4c6d172
Anders Refsahl <arefsahl@gmail.com>
2024-11-08 10:47:00 +0100

    Forsøk på å fikse kibana link fra logstash til Applikasjonslogger
    Co-authored-by: hakon.roed@nav.no

--- app/services/kibana.server.ts
+++ app/services/kibana.server.ts
@@ -14,5 +14,5 @@ export function kibanaLink(behandling: BehandlingDto) {
 
   const refreshInterval = `(refreshInterval:(pause:!t,value:0),time:(from:'${startTime}',to:'${endTime}'))`
   const query = `(language:kuery,query:'application:%22${application}%22%20AND%20x_behandlingId:%22${behandling.behandlingId}%22')`
-  return `https://logs.adeo.no/app/kibana#/discover?_g=${refreshInterval}&_a=(columns:!(level,message),grid:(columns:(level:(width:63))),index:'Applikasjonslogger',interval:auto,query:${query},sort:!(!('@timestamp',desc)))`
+  return `https://logs.adeo.no/app/kibana#/discover?_g=${refreshInterval}&_a=(columns:!(level,message),grid:(columns:(level:(width:63))),index:'96e648c0-980a-11e9-830a-e17bbd64b4db',interval:auto,query:${query},sort:!(!('@timestamp',desc)))`
 }
