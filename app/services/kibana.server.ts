cc3aba4c4ae0a0b4682dfb6d26e01331a03eb445
Anders Refsahl <arefsahl@gmail.com>
2024-11-11 08:57:28 +0100

    Kibanalink: fjerner timezone igjen

--- app/services/kibana.server.ts
+++ app/services/kibana.server.ts
@@ -1,22 +1,13 @@
 import type { BehandlingDto } from '~/types'
-import { utcToZonedTime } from 'date-fns-tz'
 import { env } from '~/services/env.server'
 
 export function kibanaLink(behandling: BehandlingDto) {
   const application = env.penApplication
-  const timeZone = 'Europe/Oslo'
   const opprettetDate = new Date(behandling.opprettet)
   const sisteKjoringDate = new Date(behandling.sisteKjoring)
   const femMinutter = 5 * 60 * 1000
-
-  const adjustToNorwegianTime = (date: Date) => {
-    return utcToZonedTime(date, timeZone)
-  }
-
-  const startTime = adjustToNorwegianTime(
-    new Date(opprettetDate.getTime() - femMinutter)).toISOString()
-  const endTime = adjustToNorwegianTime(
-    new Date(sisteKjoringDate.getTime() + femMinutter)).toISOString()
+  const startTime = new Date(opprettetDate.getTime() - femMinutter).toISOString()
+  const endTime = new Date(sisteKjoringDate.getTime() + femMinutter).toISOString()
 
   const refreshInterval = `(refreshInterval:(pause:!t,value:0),time:(from:'${startTime}',to:'${endTime}'))`
   const query = `(language:kuery,query:'application:%22${application}%22%20AND%20x_behandlingId:%22${behandling.behandlingId}%22')`
